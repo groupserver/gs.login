6d3945ea3f97cd169af0b5ccf0beaaf3073e76f5
Richard Waid <richard@iopen.net>
2007-01-30 04:11:55 +0000

    End-to-end implementation of hmac authentication. Still requires modifications to Zope to handle ongoing session auth.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSLogin/trunk%401465

--- view.py
+++ view.py
@@ -2,6 +2,8 @@
 
 '''
 import Products.Five, Globals
+import sha, hmac
+import time
 
 class GSLoginView( Products.Five.BrowserView ):
     ''' View object for logging into a groupserver site.
@@ -11,5 +13,40 @@ class GSLoginView( Products.Five.BrowserView ):
         self.context = context
         self.request = request
         self.siteInfo = Products.GSContent.interfaces.IGSSiteInfo( context )
+
+    def generateSeed( self ):
+        return sha.new(str(time.time())).hexdigest()
+
+    def passwordsEncrypted( self ):
+        return not not self.context.acl_users.encrypt_passwords
+
+    def processCredentials( self ):
+        login = self.request.get('login', '')
+        
+        user = None
+        password = None
+        if login:
+            user = self.context.acl_users.getUser(login)
+            if user:
+                password = user.get_password()
+                if self.passwordsEncrypted():
+                    # strip off the encoding declaration and the trailing '='
+                    password = password.split('}')[-1][:-1]
+
+        state = False
+        passhmac = ''
+        if user and password:       
+            passhmac = self.request.get('password', '')
+            seed = self.request.get('seed','')
+            
+            myhmac = hmac.new(password, login+password+seed, sha).hexdigest()
+
+            state = passhmac == myhmac or False
+        
+        user = not not user
+        o = password
+        password = not not password
+
+        return (state, user, password, o, passhmac)
 
 Globals.InitializeClass( GSLoginView )
