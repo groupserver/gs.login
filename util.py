a71353d80cecab607039998e559219c5856addd1
Richard Waid <richard@iopen.net>
2007-02-01 20:37:23 +0000

    Lots of bug fixes
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSLogin/trunk%401489

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger()
+
 def getDivisionObjects( context, user ):
     site_root = context.site_root()
 
@@ -28,18 +31,30 @@ def getCurrentUserDivision( context, user ):
     division_objects = getDivisionObjects(context, user)
     division_ids = map(lambda x: x.getId(), division_objects)
     
+    logger.info('divisions: %s' % division_ids)
+    
+    division_object = None
     # if we have a currentDivision, return the corresponding object
     if curr_did in division_ids:
         for obj in division_objects:
             if obj.getId() == curr_did:
-                return obj
+                division_object = obj
+                logger.info('found division 1')
+                break
     
-    # otherwise return the first division object
+    # otherwise return the first division object, preferring non-public first
     elif division_objects:
-        return division_objects[0]
+        division_object = None
+        for division_object in division_objects:
+            division_object = division_objects[0]
+            logger.info('looking at %s' % division_object.getId())
+            logger.info('%s is_public %s' % (division_object.getId(), division_object.getProperty('is_public', 0)))
+            if not division_object.getProperty('is_public', False):
+                break
     
+    logger.info('division %s' % division_object.getId())
     # otherwise, well, not much we can do
-    return None
+    return division_object
 
 def isGSUser( user ):
     try:
--- view.py
+++ view.py
@@ -8,6 +8,10 @@ import time
 from Products.XWFCore.XWFUtils import getOption
 from util import getDivisionObjects, isGSUser, getCurrentUserDivision
 
+def seedGenerator( ):
+    return sha.new(str(time.time())).hexdigest()
+
+
 class GSLoginView( Products.Five.BrowserView ):
     ''' View object for logging into a groupserver site.
 
@@ -18,11 +22,11 @@ class GSLoginView( Products.Five.BrowserView ):
         self.siteInfo = Products.GSContent.interfaces.IGSSiteInfo( context )
         self.state = None
 
-    def generateSeed( self ):
-        return sha.new(str(time.time())).hexdigest()
-
     def passwordsEncrypted( self ):
         return not not self.context.acl_users.encrypt_passwords
+
+    def generateSeed( self ):
+        return seedGenerator()
 
     def processCredentials( self ):
         login = self.request.get('login', '')
@@ -56,45 +60,71 @@ class GSLoginView( Products.Five.BrowserView ):
                 storepass = '{SHA}%s=' % password
             else:
                 storepass = password
-            
+
             self.context.cookie_authentication.credentialsChanged(user, login, storepass)
-                        
+
             came_from = self.request.get('came_from', '')
             redirect_to = ''
             if came_from:
                 redirect_to = came_from
             else:
-                current_division = getCurrentUserDivision(self.context, user)
-                if current_division:
-                    canonicalHost = getOption(current_division, 'canonicalHost')
-                else:
-                    canonicalHost = ''
-                
-                seed = self.generateSeed()
-                passhash = hmac.new(password, login+password+seed, sha).hexdigest()
-                
-                base_uri = self.request.BASE0
-                if canonicalHost:
-                    redirect_uri = 'http://%s' % canonicalHost
-                else:
-                    redirect_url = base_uri
+                redirect_to = '/login_redirect'
 
-                persist = self.request.get('__ac_persistent', '')
-                
-                if base_uri != redirect_uri:
-                    redirect_to = ('%s/login.html?login=%s&ph=%s&seed=%s&__ac_persistent=%s' %
-                                   (redirect_uri, login, passhash, seed, persist))
-                elif not password:
-                    # PPP: Need to be updated to pragmatic template
-                    redirect_to = '%s/set_password.xml' % redirect_uri
-                else:
-                    redirect_to = '%s/loggedin.html' % redirect_uri
-            
             self.request.response.redirect(redirect_to)                
 
         user = not not user
         password = not not password
         
         self.state = (state, user, password)
+
+class GSLoginRedirect( Products.Five.BrowserView ):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.siteInfo = Products.GSContent.interfaces.IGSSiteInfo( context )
+
+    def loginRedirect( self ):
+        user = self.request.AUTHENTICATED_USER
+        if user:
+            password = user.get_password()
+            login = user.getId()
+        else:
+            return self.request.response.redirect('login.html')
+
+        # if we are logging into the public site, figure out where to go
+        canonicalHost = ''
+        if self.siteInfo.siteObj.getProperty('is_public', False):        
+            current_division = getCurrentUserDivision(self.context, user)
+            if current_division:
+                canonicalHost = getOption(current_division, 'canonicalHost')
+
+        seed = seedGenerator()
+        passhash = hmac.new(password, login+password+seed, sha).hexdigest()
+        
+        base_uri = self.request.BASE0
+        if canonicalHost:
+            redirect_uri = 'http://%s' % canonicalHost
+        else:
+            redirect_uri = base_uri
+
+        persist = self.request.get('__ac_persistent', '')
+
+        if base_uri != redirect_uri:
+            # NEVER set cookie credentials for the public site or we will be
+            # trapped in redirect hell
+            if self.siteInfo.siteObj.getProperty('is_public', False):            
+                self.request.response.expireCookie( self.context.cookie_authentication.auth_cookie )
+            redirect_to = ('%s/login.html?login=%s&ph=%s&seed=%s&__ac_persistent=%s' %
+                            (redirect_uri, login, passhash, seed, persist))
+        elif not password:
+            # PPP: Need to be updated to pragmatic template
+            redirect_to = '%s/set_password.xml' % redirect_uri
+        else:
+            redirect_to = '%s/' % redirect_uri
+
+                        
+
+        
+        self.request.response.redirect(redirect_to)
 
 Globals.InitializeClass( GSLoginView )
