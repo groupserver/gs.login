aa6948d63d1f94cd0722d406c90b1508e06bc9d6
Richard Waid <richard@iopen.net>
2009-10-04 00:23:07 +1300

    Rearranged into egg form
    --HG--
    rename : .hgignore => Products/GSLogin/.hgignore
    rename : __init__.py => Products/GSLogin/__init__.py
    rename : browser/__init__.py => Products/GSLogin/browser/__init__.py
    rename : browser/templates/__init__.py => Products/GSLogin/browser/templates/__init__.py
    rename : browser/templates/default/__init__.py => Products/GSLogin/browser/templates/default/__init__.py
    rename : browser/templates/default/forgottenpassword.pt => Products/GSLogin/browser/templates/default/forgottenpassword.pt
    rename : browser/templates/default/loggedin.pt => Products/GSLogin/browser/templates/default/loggedin.pt
    rename : browser/templates/default/loggedout.pt => Products/GSLogin/browser/templates/default/loggedout.pt
    rename : browser/templates/default/login.pt => Products/GSLogin/browser/templates/default/login.pt
    rename : browser/templates/default/register.pt => Products/GSLogin/browser/templates/default/register.pt
    rename : browser/templates/default/sentpassword.pt => Products/GSLogin/browser/templates/default/sentpassword.pt
    rename : configure.zcml => Products/GSLogin/configure.zcml
    rename : interfaces.py => Products/GSLogin/interfaces.py
    rename : loginaudit.py => Products/GSLogin/loginaudit.py
    rename : util.py => Products/GSLogin/util.py
    rename : view.py => Products/GSLogin/view.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.98'
+
+setup(name='Products.GSLogin',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Richard Waid',
+      author_email='richard@iopen.net',
+      url='http://groupserver.org',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
