9f865e1394ba1fffa787718e64d50c7e915842aa
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-23 14:47:19 +1000

    Removing the Python 2.4 code from the utils.

--- gs/login/util.py
+++ gs/login/util.py
@@ -1,22 +1,11 @@
 # coding=utf-8
-from types import BuiltinFunctionType
-try:
-    # Python 2.6
-    from hashlib import sha1 as sha
-    seed_generator = sha
-except ImportError:
-    # --=mpj17=-- Question: Do we need to support Python 2.4?
-    # Python 2.4
-    import sha
-    seed_generator = sha.sha
-assert type(seed_generator) == BuiltinFunctionType,\
-    'Did not create the seed generator'
+from hashlib import sha1 as sha
 import time, random, logging
 
 logger = logging.getLogger()
 
 def seedGenerator( ):
-    s = seed_generator(str(time.time())+str(random.random()))
+    s = sha(str(time.time())+str(random.random()))
     retval = s.hexdigest()
     assert retval
     return retval
