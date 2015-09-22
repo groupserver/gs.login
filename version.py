d3d2ae093b46ff2d7cf493cce55580f7f856af72
Michael JasonSmith <mpj17@onlinegroups.net>
2015-09-22 13:28:31 -0700

    Not a release

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 version = '2.1.0'
-release = True
+release = False
 
 #-----------------------------------------------------------------------------#
 import sys
@@ -61,7 +61,7 @@ def get_version():
         if release:
             version_string = version
         else:
-            version_string = "%s.dev%s+%s" % (version, datestring, globalid)
+            version_string = "%s.dev%s" % (version, datestring)
 
     except (CommandError, ValueError, TypeError):
         # --=mpj17=-- Usually because we are building out a source-egg, rather
