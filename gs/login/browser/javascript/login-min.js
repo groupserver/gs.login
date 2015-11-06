10fc1cbeaee3b215877e6bfaa8264877836bcbc3
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-06 15:11:47 -0800

    Minify

--- gs/login/browser/javascript/login-min.js
+++ gs/login/browser/javascript/login-min.js
@@ -1,8 +1,2 @@
-jQuery.noConflict();function gs_login_submit(e){var d=null,c=null,g=null,b=null,a=null,f=null;
-d=jQuery("#login-form");c=jQuery("#password").val();g=jQuery("#ep").val();if(g=="True"){c=b64_sha1(c)
-}b=jQuery("#login").val();a=jQuery("#seed").val();f=hex_hmac_sha1(c,(b+c+a));jQuery("#ph").val(f);
-jQuery("#password").val("")}function gs_login_init(){jQuery("#login-form").submit(gs_login_submit)
-}function gs_login_toggle_init(){var a=null;a=GSProfilePasswordToggle("#password","#gs-login-password-toggle-widget")
-}jQuery(window).load(function(){jQuery("#login").focus();gsJsLoader.with_module("/++resource++crypto-20130402/sha1-min.js",gs_login_init);
-gsJsLoader.with_module("/++resource++gs-profile-password-toggle-min-20130516.js",gs_login_toggle_init)
-});
+'use strict';jQuery.noConflict();function gs_login_submit(a){var b=a=null,c=b=null;a=null;jQuery("#login-form");a=jQuery("#password").val();b=jQuery("#ep").val();"True"==b&&(a=b64_sha1(a));b=jQuery("#login").val();c=jQuery("#seed").val();a=hex_hmac_sha1(a,b+a+c);jQuery("#ph").val(a);jQuery("#password").val("")}function gs_login_init(){jQuery("#login-form").submit(gs_login_submit)}function gs_login_toggle_init(){GSProfilePasswordToggle("#password","#gs-login-password-toggle-widget")}
+jQuery(window).load(function(){jQuery("#login").focus();gsJsLoader.with_module("/++resource++crypto-20130402/sha1-min.js",gs_login_init);gsJsLoader.with_module("/++resource++gs-profile-password-toggle-min-20130516.js",gs_login_toggle_init)});
