65ab65502978010033738d4f524b33d827912429
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-09 14:16:51 -0800

    Metadata update

--- README.rst
+++ README.rst
@@ -10,42 +10,42 @@ The Sign In page
 :Date: 2013-04-02
 :Organization: `GroupServer.org`_
 :Copyright: This document is licensed under a
-  `Creative Commons Attribution-Share Alike 3.0 New Zealand License`_
-  by `OnlineGroups.Net`_.
+ `Creative Commons Attribution-Share Alike 4.0 International License`_
+  by `OnlineGroups.net`_.
+
+..  _Creative Commons Attribution-Share Alike 4.0 International License:
+    http://creativecommons.org/licenses/by-sa/4.0/
 
 Introduction
 ============
 
-This product is responsible to showing the Login page for a GroupServer
-site. The page can be shown in two ways:
+This product is responsible to showing the Login page for a
+GroupServer site. The page can be shown in two ways:
 
 #. The user explicitly visits ``login.html``, or
-#. A Permission Denied error is  generated.
+#. A Permission Denied error is generated.
 
-In the latter case a login page is shown if the anonymous-user generated
-the error, otherwise a Permission Denied error is shown. The error
-has a link to the support email for the site, so the user can easily
-contact support if he or she thinks she saw the error in error.
+In the latter case a login page is shown if the anonymous-user
+generated the error, otherwise a Permission Denied error is
+shown. The error has a link to the support email for the site, so
+the user can easily contact support if they think that they saw
+the error in error.
 
-JavaScript
-==========
-
-`A JavaScript library`_ by Paul Johnston is used to encrypt the login. It
-is imported by the resource ``gs-login-20130402.js``.
+`A JavaScript library`_ by Paul Johnston is used to encrypt the
+login. It is imported by the resource ``gs-login-20151106.js``.
 
 Resources
 =========
 
-- Code repository: https://source.iopen.net/groupserver/gs.login
-- Questions and comments to http://groupserver.org/groups/development
+- Code repository: https://github.com/groupserver/gs.login
+- Questions and comments to
+  http://groupserver.org/groups/development
 - Report bugs at https://redmine.iopen.net/projects/groupserver
 
 .. _GroupServer: http://groupserver.org/
 .. _GroupServer.org: http://groupserver.org/
 .. _OnlineGroups.Net: https://onlinegroups.net
 .. _Michael JasonSmith: http://groupserver.org/p/mpj17
-.. _Creative Commons Attribution-Share Alike 3.0 New Zealand License:
-   http://creativecommons.org/licenses/by-sa/3.0/nz/
 .. _A JavaScript library: <http://pajhome.org.uk/crypt/md5/>`_
 
 ..  LocalWords:  ISiteHomeMain groupserver GroupListContent nz html
--- setup.py
+++ setup.py
@@ -12,58 +12,66 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
+import sys
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.login',
 version = get_version()
 
-setup(name='gs.login',
-      version=version,
-      description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.rst")).read(),
-      classifiers=[
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
+setup(
+    name=name,
+    version=version,
+    description="The Sign In page for GroupServer",
+    long_description=long_description,
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
         "Framework :: Zope2",
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='login authentication',
-      author='Richard Waid',
-      author_email='richard@onlinegroups.net',
-      maintainer='Michael JasonSmith',
-      maintainer_email='mpj17@onlinegroups.net',
-      url='https://source.iopen.net/groupserver/gs.login/',
-      license='ZPL 2.1',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['gs'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'pytz',
-          'zope.browserpage',
-          'zope.browserresource',
-          'zope.component',
-          'zope.interface',
-          'zope.tal',
-          'zope.tales',
-          'AccessControl',
-          'gs.content.base',
-          'gs.content.layout',
-          'gs.core',
-          'gs.database',
-          'Products.CustomUserFolder',
-          'Products.XWFCore',
-          'Products.GSAuditTrail',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='login authentication',
+    author='Richard Waid',
+    author_email='richard@onlinegroups.net',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
+    url='https://github.com/groupserver/{0}'.format(name),
+    license='ZPL 2.1',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'pytz',
+        'zope.browserpage',
+        'zope.browserresource',
+        'zope.component',
+        'zope.interface',
+        'zope.tal',
+        'zope.tales',
+        'AccessControl',
+        'gs.content.base',
+        'gs.content.layout',
+        'gs.core',
+        'gs.database',
+        'Products.CustomUserFolder',
+        'Products.XWFCore',
+        'Products.GSAuditTrail',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """, )
